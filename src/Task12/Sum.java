c125272aadde9f64607ba1f6b8c28f85785a048f
Vadim Arefiev <vadimarefev@i.ua>
2022-12-12 22:59:41 +0200

    finish

--- src/Task12/Sum.java
+++ src/Task12/Sum.java
@@ -1,8 +1,13 @@
 package Task12;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sum {
     public static void main(String[] args) {
         System.out.println(GetSum(8, 1));
+        System.out.println(nbDig(10, 1));
     }
 
     public static int GetSum(int a, int b) {
@@ -22,6 +27,30 @@ public class Sum {
         }
         return sum;
     }
-}
+
+
+    public static int nbDig(int n, int d) {
+        List list = new ArrayList();
+        int sum = 0;
+        if(n >= 0){
+            for(int i = 0; i <= n; i++){
+                list.add(i*i);
+            }
+            char[] arr = list.toString().toCharArray();
+            for(Object s: arr){
+                if(s.toString().contains(Integer.toString(d))){
+                    sum += 1;
+                }
+            }
+        }
+        return sum;
+            }
+        }
+
+
+
+
+
+
 
 
